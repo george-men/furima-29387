20772cb2499a64daaebe4d611a686caacda6a117
george-men <jyouoshikiri@gmail.com>
2020-09-18 17:36:05 +0900

    リダイレクト機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
 
@@ -10,4 +12,11 @@ class ItemsController < ApplicationController
 
   def edit
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
