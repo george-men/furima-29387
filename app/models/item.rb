f66846a4886725bbd75e0b92ec908d9d900708b3
george-men <jyouoshikiri@gmail.com>
2020-09-20 21:48:59 +0900

    プルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :product_status
+  belongs_to_active_hash :fee_burden
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_day
+
   belongs_to :user
-  
+
+  with_options presence: true do
+    validates :product_name
+    validates :explanation
+    validates :category_id, numericality: { other_than: 1 }
+    validates :product_status_id, numericality: { other_than: 1 }
+    validates :fee_burden_id, numericality: { other_than: 1 }
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :shipping_days_id, numericality: { other_than: 1 }
+    validates :price 
+  end
 end
