b9cd94dc1d2fb9e6d939881ad75486073f7641a6
george-men <jyouoshikiri@gmail.com>
2020-09-24 18:37:03 +0900

    削除機能実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
+  end
+
   def show
   end
 
