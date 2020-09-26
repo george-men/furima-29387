80e5557e9cb8c0d6f10aad3d0ae6b29eb46087bd
george-men <jyouoshikiri@gmail.com>
2020-09-26 18:50:56 +0900

    決済機能のJS編集、環境変数設定、createアクションとpay_itemアクションの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,29 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:postal_number, :prefecture_id, :city, :address, :building_name, :phone_number, :token)
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:postal_number, :prefecture_id, :city, :address, :building_name, :phone_number],  # 商品の送り先など
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 
 end
